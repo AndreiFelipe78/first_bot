f4182c0593530b8a7b5ddcdc988bf57a8c57c2f4
Felipe Santos <andrei@pop-os.localdomain>
2022-04-13 20:15:18 -0300

    bot funcionando

--- meu_bot.py
+++ meu_bot.py
@@ -1,9 +1,8 @@
-
 from shutil import move
 import telebot
-from telebot import types
 from credencials import chave_API
 from PIL import Image
+import os
 
 
 bot = telebot.TeleBot(chave_API)
@@ -18,7 +17,39 @@ def opcao2(mensagem):
 
 @bot.message_handler(commands=["opcao3"])
 def opcao3(mensagem):
-    bot.reply_to(mensagem, "Por favor, envie a(s) foto(s) para a conversão. Ao finalizar use o comando /gerar_pdf")
+    bot.reply_to(mensagem, "Por favor, envie a(s) foto(s) para a conversão. Ao finalizar, use o comando /gerar_pdf")
+    @bot.message_handler(content_types='photo')
+    def image_downloader(mensagem):
+        raw = mensagem.photo[2].file_id
+        path = raw+".jpg"
+        file_info = bot.get_file(raw)
+        downloaded_file = bot.download_file(file_info.file_path)
+        with open(path,'wb') as new_file:
+            new_file.write(downloaded_file)   
+        original = '/home/andrei/Documents/new_bot/{}'.format(path)
+        target = '/home/andrei/Documents/new_bot/pictures/'
+        move(original,target)
+@bot.message_handler(commands=["gerar_pdf"])
+def gerar_pdf(mensagem):
+    dir = '/home/andrei/Documents/new_bot/pictures/'
+    file_names = os.listdir(dir)
+    if len(file_names) == 0:
+        bot.send_message(mensagem.chat.id, "Olhei aqui e acho que você não enviou nenhuma foto {}. \nPreciso de ovos de para fazer omelete. \nEnvie as fotos e pressione /gerar_pdf".format(mensagem.chat.first_name))
+    else:
+        photo_list = []
+        photo_rgb = []
+        for photo in file_names:
+            photo_list.append(Image.open(r'/home/andrei/Documents/new_bot/pictures/{}'.format(photo)))
+        for item in photo_list:
+            photo_rgb.append(item.convert('RGB'))
+        photo_rgb[0].save(r'{}my_file.pdf'.format(dir), save_all=True, append_images=photo_rgb)
+        bot.send_message(mensagem.chat.id, 'Gerando PDF...')
+        bot.send_document(mensagem.chat.id, document=open('/home/andrei/Documents/new_bot/pictures/my_file.pdf', 'rb'))
+        bot.send_message(mensagem.chat.id, 'as fotos enviadas e os dados gerados serão apagados agora.')
+        file_names = os.listdir(dir)
+        for item in file_names:
+            os.remove('{}{}'.format(dir,str(item)))
+        
 
 @bot.message_handler(content_types='text')
 def resposta_padrao(mensagem):
@@ -31,22 +62,16 @@ def resposta_padrao(mensagem):
     Responder qualquer outra coisa não vai funcionar, clique em uma das opções"""
     bot.send_message(mensagem.chat.id, texto)
 
-@bot.message_handler(content_types='photo')
-def image_converter(mensagem):
-    raw = mensagem.photo[2].file_id
-    path = raw+".jpg"
-    file_info = bot.get_file(raw)
-    downloaded_file = bot.download_file(file_info.file_path)
-    with open(path,'wb') as new_file:
-        new_file.write(downloaded_file)   
-    original = '/home/andrei/Documents/new_bot/{}'.format(path)
-    target = '/home/andrei/Documents/new_bot/pictures/'
-    move(original,target)
-    original = '{}'.format(path)
 
-    image_1 = Image.open(r'{}'.format(str(original)))
-    im_1 = image_1.convert('RGB')
-    im_1.save(r'{}'.format(target+raw+'.pdf'))
+    
+
+
+
+
+
+    # image_1 = Image.open(r'{}'.format(str(original)))
+    # im_1 = image_1.convert('RGB')
+    # im_1.save(r'{}'.format(target+raw+'.pdf'))
     
 bot.polling()
 
